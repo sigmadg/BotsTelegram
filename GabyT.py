b93155c45c0d1a6e0add381f8e36f155bc6a52e7
anaordonez <ana.ordonez@elektra.com.mx>
2023-05-04 17:57:11 -0600

    Agenda Telegram

--- GabyT.py
+++ GabyT.py
@@ -1,61 +1,73 @@
-import logging
-from datetime import datetime
-from telegram.ext import Updater, CommandHandler, CallbackContext
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import datetime
+import json
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-API_TOKEN = "5799434637:AAEeUVstwBaH02-Vv2J5O5uMSpaUHhkmBfQ"
-
-eventos_recurrentes = {}
-actividades_pendientes = []
+TOKEN = '5799434637:AAEeUVstwBaH02-Vv2J5O5uMSpaUHhkmBfQ'
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="¡Hola! Soy tu agenda para eventos recurrentes y actividades pendientes.")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hola, ¿en qué puedo ayudarte?")
 
-def agregar_evento_recurrente(update, context):
-    try:
-        dia = context.args[0].lower()
-        evento = ' '.join(context.args[1:])
-        
-        if dia not in eventos_recurrentes:
-            eventos_recurrentes[dia] = []
-        eventos_recurrentes[dia].append(evento)
-        
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Evento '{evento}' agregado al día {dia}.")
-    except (IndexError, ValueError):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Uso: /agregar_evento_recurrente <día> <evento>")
+def set_task(update, context):
+    # Obtener la tarea y la fecha del mensaje del usuario
+    message = update.message.text
+    task, date = message.split(" ", 1)
 
-def ver_eventos_recurrentes(update, context):
-    dia = datetime.today().strftime('%A').lower()
-    
-    if dia in eventos_recurrentes and eventos_recurrentes[dia]:
-        eventos_hoy = "\n".join(eventos_recurrentes[dia])
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Eventos recurrentes para hoy:\n{eventos_hoy}")
+    # Convertir la fecha en un objeto datetime
+    date = datetime.datetime.strptime(date, '%d/%m/%Y').date()
+
+    # Obtener la lista de tareas existentes para esta fecha
+    task_list = context.user_data.get(str(date), [])
+
+    # Agregar la nueva tarea a la lista
+    task_list.append(task)
+
+    # Actualizar el diccionario de tareas del usuario
+    context.user_data[str(date)] = task_list
+
+    # Confirmar la creación de la tarea
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Tarea '{task}' creada para el {date}.")
+
+
+def show_tasks(update, context):
+    # Obtener el diccionario de tareas del usuario
+    task_dict = context.user_data
+
+    # Convertir el diccionario en una cadena JSON
+    task_json = json.dumps(task_dict, indent=4)
+
+    # Enviar la cadena JSON como un mensaje al usuario
+    context.bot.send_message(chat_id=update.effective_chat.id, text=task_json)    
+
+def get_tasks(update, context):
+    # Obtener la fecha del mensaje del usuario
+    date_str = update.message.text
+
+    # Convertir la fecha en un objeto datetime
+    date = datetime.datetime.strptime(date_str, '%d/%m/%Y').date()
+
+    # Obtener la lista de tareas existentes para esta fecha
+    task_list = context.user_data.get(str(date), [])
+
+    # Si no hay tareas para esta fecha, mostrar un mensaje al usuario
+    if not task_list:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"No hay tareas para el {date}.")
+    # Si hay tareas para esta fecha, mostrar la lista de tareas al usuario
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="No hay eventos recurrentes para hoy.")
-
-def agregar_actividad_pendiente(update, context):
-    actividad = ' '.join(context.args)
-    actividades_pendientes.append(actividad)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Actividad '{actividad}' agregada a la lista de pendientes.")
-
-def ver_actividades_pendientes(update, context):
-    if actividades_pendientes:
-        lista_pendientes = "\n".join(actividades_pendientes)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Lista de actividades pendientes:\n{lista_pendientes}")
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="No hay actividades pendientes.")
+        task_str = "\n".join([f"- {task}" for task in task_list])
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Tareas para el {date}:\n{task_str}")
 
 def main():
-    updater = Updater(API_TOKEN)
+    # Crear un objeto de actualización y un objeto de despachador
+    updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
+    # Agregar manejadores de comandos y mensajes
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("agregar_evento_recurrente", agregar_evento_recurrente, pass_args=True))
-    dispatcher.add_handler(CommandHandler("ver_eventos_recurrentes", ver_eventos_recurrentes))
-    dispatcher.add_handler(CommandHandler("agregar_actividad_pendiente", agregar_actividad_pendiente, pass_args=True))
-    dispatcher.add_handler(CommandHandler("ver_actividades_pendientes", ver_actividades_pendientes))
+    dispatcher.add_handler(MessageHandler(Filters.regex(r'^\w+ \d{1,2}/\d{1,2}/\d{4}$'), set_task))
+    dispatcher.add_handler(MessageHandler(Filters.regex(r'^\d{1,2}/\d{1,2}/\d{4}$'), get_tasks))
 
+    # Iniciar el bot
     updater.start_polling()
     updater.idle()
 
